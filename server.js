a5a0cfed18cdb99ef2a24f785c883dee391817c2
rekamanoharan <rekamano15@gmail.com>
2024-01-12 15:49:16 +0530

    Blog app-backend

--- server.js
+++ server.js
@@ -24,14 +24,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use(
-  "https://master--comforting-praline-757aaa.netlify.app/api/v1/user",
-  userRoutes
-);
-app.use(
-  "https://master--comforting-praline-757aaa.netlify.app/api/v1/blog",
-  blogRoutes
-);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
 // Port
 const PORT = process.env.PORT || 8080;
